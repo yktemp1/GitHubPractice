262ffb50fc299b9a57fecbdc8bf22d750d8a237a
yktemp2 <ebabokz@theeasymail.com>
2019-07-09 18:14:31 +0530

    add Train Class

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,29 @@
+class Train{
+    constructor(speed){
+        this.speed = speed;
+        this.t = 0;
+    }
+
+    choo(){
+        console.log("chooo... chooo....");
+    }
+
+    display(){
+        if(this.t < 100){
+            noStroke();
+            fill("#f00");
+            rect(100 + this.speed * this.t, 100, 100);
+        }
+    }
+}
+
+let train;
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    
+    train = new Train(100);
 }
 
 function draw(){
     background("#333");
-    rect(0,0,width/2,height/2);
+    TransitionEvent.display();
 }
